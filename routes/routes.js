e6317a3e47cca0de5403799b93999ea4e02b4d82
Mateo Balcorta <misterrattail@gmail.com>
2018-10-05 08:43:16 -0700

    Remove post route to send mail.

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 const postFccComment = require('../fccComment.js');
-const sendEmail = require('../confirmationEmail.js'); 
 
 const appRouter = (app) => {
   app.get('/', (req, res) => {
@@ -10,13 +9,6 @@ const appRouter = (app) => {
     postFccComment(req.body)
       .then(answer => res.send(answer))
       .catch(error => console.log(error));
-  });
-
-  app.post('/sendEmail', (req, res) => {
-    const { toEmail } = req.body;
-    sendEmail(toEmail)
-      .then(response => res.send(response))
-      .catch(error => console.log(error))
   });
 };
 module.exports = appRouter;
