912d5425ed6d1fbbb08a505b5b2fb2a1320c4aa6
Mateo Balcorta <misterrattail@gmail.com>
2018-10-11 10:13:57 -0700

    Remove async/await to catch promises.

--- fccComment.js
+++ fccComment.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 const { parseString } = require('xml2js');
-let keys = ''; 
-if(!process.env.fccCommentKey) {
+
+let keys = '';
+if (!process.env.fccCommentKey) {
   keys = require('./config/key.js');
 }
 
-const getStateAndCity = zip => new Promise(async (resolve, reject) => {
+const getStateAndCity = zip => new Promise((resolve, reject) => {
   axios.get('https://secure.shippingapis.com/ShippingAPI.dll?', {
     params: {
       API: 'CityStateLookup',
@@ -16,69 +17,72 @@ const getStateAndCity = zip => new Promise(async (resolve, reject) => {
       const xml = response.data;
 
       parseString(xml, (err, result) => {
-        const { State, City } = result.CityStateLookupResponse.ZipCode[0];
-        resolve({
-          state: State[0],
-          city: City[0],
-        });
+        const errorMessage = result.CityStateLookupResponse.ZipCode[0].Error;
+        if (errorMessage) {
+          throw new Error(`Error Message: ${errorMessage[0].Description[0]}, zipcode: ${zip}`);
+        } else {
+          const { State, City } = result.CityStateLookupResponse.ZipCode[0];
+          resolve({
+            state: State[0],
+            city: City[0],
+          });
+        }
       });
     })
     .catch((error) => {
-      console.log('error from line 27 fccComment.js ', error, 'zipcode: ', zip)
       reject(error);
     });
 });
 
-const postFccComment = reqBody => new Promise(
-  async (resolve, reject) => {
-    const {
-      first_name,
-      last_name,
-      email,
-      address1,
-      zip,
-      fcc_comment,
-    } = reqBody;
+const postFccComment = (reqBody) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    address1,
+    zip,
+    fcc_comment,
+  } = reqBody;
 
-    const cityStateObj = await getStateAndCity(zip);
-    const { state, city } = cityStateObj;
-    const key = process.env.fccCommentKey ? process.env.fccCommentKey : keys.fccKey;
-
-    axios({
-      method: 'post',
-      url: `https://publicapi.fcc.gov/ecfs/filings?api_key=${key}`,
-      data: {
-        proceedings: [
-          {
-            bureau_code: 'WTB',
-            bureau_name: 'Wireless Telecommunications Bureau',
-            name: '18-197',
+  return new Promise((resolve, reject) => {
+    getStateAndCity(zip).then((cityStateObj) => {
+      const { state, city } = cityStateObj;
+      const key = process.env.fccCommentKey ? process.env.fccCommentKey : keys.fccKey;
+      axios({
+        method: 'post',
+        url: `https://publicapi.fcc.gov/ecfs/filings?api_key=${key}`,
+        data: {
+          proceedings: [
+            {
+              bureau_code: 'WTB',
+              bureau_name: 'Wireless Telecommunications Bureau',
+              name: '18-197',
+            },
+          ],
+          filers: [
+            {
+              name: `${first_name} ${last_name}`,
+            },
+          ],
+          contact_email: email,
+          addressentity: {
+            address_line_1: address1,
+            city,
+            state,
+            zip_code: zip,
           },
-        ],
-        filers: [
-          {
-            name: `${first_name} ${last_name}`,
-          },
-        ],
-        contact_email: email,
-        addressentity: {
-          address_line_1: address1,
-          city,
-          state,
-          zip_code: zip,
+          text_data: fcc_comment,
+          express_comment: 1,
         },
-        text_data: fcc_comment,
-        express_comment: 1,
-      },
-    })
-      .then((response) => {
+      }).then((response) => {
         resolve(response.data.status);
-      })
-      .catch((error) => {
-        console.log('error from line 78 fccComment.js ', error)
-        reject(error);
+      }).catch((error) => {
+        throw new Error(error);
       });
-  },
-);
+    }).catch((error) => {
+      reject(error);
+    });
+  });
+};
 
 module.exports = postFccComment;
